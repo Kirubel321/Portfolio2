4f5ef01daff1c20157bcdc03656fbe772a89c233
Kirubel321 <72298742+Kirubel321@users.noreply.github.com>
2022-05-05 11:48:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var skill = document.getElementById('skill_');
+skill.addEventListener('click',reload_)
+var home = document.getElementById('home_')
+home.addEventListener('click',reload_)
+
+function reload_(){
+    location.reload();
+}
